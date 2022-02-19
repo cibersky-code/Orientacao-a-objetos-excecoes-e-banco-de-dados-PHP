fa738cfae06870705760b5d26732b73ea73cd705
cibersky-code <cibersky@gmail.com>
2022-02-19 20:14:06 -0300

    atualização

--- Introdução a banco de dados com PHP/PDO/SRC/Produto.php
+++ Introdução a banco de dados com PHP/PDO/SRC/Produto.php
@@ -22,9 +22,53 @@ class Produto
 
 	}
 
-	public function list()
+	public function list() : array 
 	{
-		return true;
+	$sql = 'select * fron produtos';
+
+	$produtos = [];
+
+	foreach ($this->conexao->query($sql) as $key => $value) {
+	array_push($produtos, $value);
+	}
+	
+
+	public function insert(string $descricao) : int 
+	{
+	$sql = 'insert into produtos(descricao) value(?)';
+
+	$prepare = $this->conexao->prepare($sql);
+
+	$prepare->bindParam(1, $descricao);
+	$prepare->execute();
+
+	return prepare->rowCount();
+	}
+	
+	public function update(string $descricao, int $id) : int 
+	{
+	$sql = 'upadate produtos set descrição = ? where id = ?';
+
+	$prepare = $this->conexao->prepare($sql);
+
+	$prepare->blindParam(1, $descricao);
+	$prepare->blindParam(2, $id);
+
+	$prepare = execute();
+
+
+	return prepare->rowCount();
 	}
 
+	public function delete(int $id) : int 
+	{
+	$sql = 'dalete from produtos where id = ?';
+
+	$prepare = $this->conexao->prepare($sql);
+
+	$prepare->blindParam(1, $id);
+	$prepare = execute();
+
+	return prepare->rowCount();
+	}
 }
--- Introdução a banco de dados com PHP/PDO/SRC/index.php
+++ Introdução a banco de dados com PHP/PDO/SRC/index.php
@@ -3,10 +3,64 @@
 /**
  *@author cibersky;
 */
-require 'Produto.php':
 
 declare(strict_types=1);
 
+require 'Produto.php':
+
 $produto = new Produto();
 
-$produto->list();
+switch ($_GET['operacao']) {
+
+    case 'list':
+
+        echo '<h3>...Produtos: <h3';
+        foreach ($produto->list() as $value) {
+            array_push($produtos, $value);
+        }
+        break;
+    
+    case 'insert':
+        
+        $status = $produto->insert('produto teste');
+
+        if (! $status) {
+        echo 'não foi possivel realizar a operação'; 
+        retun false;   
+        }
+        
+        echo 'registro inserido com sucesso';
+
+
+        break;
+
+    case 'update':
+        $status = $produto->update('produto alterado', 4);
+
+        if (! $status) {
+        echo 'não foi possivel realizar a operação'; 
+        retun false;   
+        }
+        
+        echo 'registro atualizado com sucesso';
+
+        break;
+
+    case 'delete':
+        
+        $status = $produto->delete(3);
+
+         if (! $status) {
+        echo 'não foi possivel realizar a operação'; 
+        retun false;   
+        }
+        
+        echo 'registro removido com sucesso';
+
+        break;
+    
+    default:
+        
+        break;
+
+}
